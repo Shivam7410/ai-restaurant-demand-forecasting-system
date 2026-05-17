08191e63e83dd0dd5aab03a4046c20405d9568ee
Shivam7410 <shivamnagar815@gmail.com>
2026-05-17 16:48:14 +0530

    model: improved forecasting accuracy using lag and rolling features

--- src/train_model.py
+++ src/train_model.py
@@ -7,14 +7,32 @@ df = pd.read_csv('data/restaurant_sales_data.csv')
 
 df['date'] = pd.to_datetime(df['date'])
 
+# Time Features
 df['day_of_week'] = df['date'].dt.dayofweek
-
 df['is_weekend'] = df['day_of_week'].apply(lambda x: 1 if x >= 5 else 0)
 
-X = df[['day_of_week', 'is_weekend']]
+# Lag Features
+df = df.sort_values('date')
+
+df['lag_1'] = df['quantity_sold'].shift(1)
+df['lag_7'] = df['quantity_sold'].shift(7)
+
+# Rolling Average
+df['rolling_mean_7'] = df['quantity_sold'].rolling(window=7).mean()
+
+# Remove Missing Values
+df = df.dropna()
+
+# Features and Target
+X = df[['day_of_week',
+        'is_weekend',
+        'lag_1',
+        'lag_7',
+        'rolling_mean_7']]
 
 y = df['quantity_sold']
 
+# Time-Series Split
 train_size = int(len(df) * 0.8)
 
 X_train = X.iloc[:train_size]
@@ -23,15 +41,21 @@ X_test = X.iloc[train_size:]
 y_train = y.iloc[:train_size]
 y_test = y.iloc[train_size:]
 
+# Model
 model = LinearRegression()
 
+# Training
 model.fit(X_train, y_train)
 
+# Predictions
 predictions = model.predict(X_test)
 
+# Evaluation
 mae = mean_absolute_error(y_test, predictions)
 
 rmse = root_mean_squared_error(y_test, predictions)
+
+print("Improved Model Results")
 
 print("MAE:", mae)
 
